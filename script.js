8d570fe508a84b9659512489f498cad52d9c7b5d
shalanubale <ubaleshalan1836@gmail.com>
2025-10-09 02:26:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-//your JS code here. If required.
 function secondHighest(arr) {
-    if (arr.length < 2) return -Infinity;
+  if (arr.length < 2) return -Infinity; // Less than 2 elements
 
-    let max = -Infinity;
-    let secondMax = -Infinity;
+  let first = -Infinity;
+  let second = -Infinity;
 
-    for (let num of arr) {
-        if (num > max) {
-            secondMax = max;
-            max = num;
-        } else if (num < max && num > secondMax) {
-            secondMax = num;
-        }
+  for (let num of arr) {
+    if (num > first) {
+      second = first;
+      first = num;
+    } else if (num > second && num < first) {
+      second = num;
     }
+  }
 
-    return secondMax === -Infinity ? -Infinity : secondMax;
+  return second;
 }
+
+// ---- Test Cases ----
+console.log(secondHighest([5, 1, 2, 3, 4]));       // 4
+console.log(secondHighest([-1, -2, -3, -4, -5])); // -2
+console.log(secondHighest([]));
